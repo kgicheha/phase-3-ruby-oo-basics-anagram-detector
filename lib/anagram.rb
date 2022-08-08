d01e8ceab4e47ba5ef98c123d12cd9785608d589
Kevin Gicheha <gichehakevin@gmail.com>
2022-08-08 14:19:45 -0700

    commit tree d11dcdcf6acee8980b0be5393eabaa104c57d797

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @name = word
+    end
+
+    def match(array)
+        array.filter {|x| x.split("").sort == @name.split("").sort}
+    end
+end
